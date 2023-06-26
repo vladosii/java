621fcd17e856e616f8b2241512e49840d14fe8f6
Vlad Tsapok <vlad.capok@mail.ru>
2023-06-26 11:25:49 +0300

    Comp hw_1

--- hw_1/hw_1_2.java
+++ hw_1/hw_1_2.java
@@ -1,20 +1,18 @@
 // Вывести все простые числа от 1 до 1000. Просто число это когда число делится на 2 и на само себя
 
-// public class hw_1_2 {
+public class hw_1_2 {
 
-//     public static void main(String[] args) {
-//         int number = 1000;
-//         for (int i = 2; i < number; i++) {
-//             boolean a = true;
-//             int q = (int) Math.sqrt(i);
-//             for (int j = 2; j <= q; j++){
-//                 a = false;
-//                 break;
-//             }
-//             if (a){
-//                 System.out.println(i);
-//             }
-//         }
-//     }
-
-// }
+    public static void main(String[] args) {
+        System.out.println("HI");
+        for(int i = 2; i < 1000; ++i){
+        int count = 0;
+        for(int j = 2; j <=i && count < 2;++j){
+            if(i%j==0){
+            ++count;
+            }
+        }
+        if(count<2)
+            System.out.println(i);
+        }
+    }
+}
