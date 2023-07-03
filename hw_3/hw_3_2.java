9d2744bc45fe696e727128ca8d8010d68fa33cdc
Vlad Tsapok <vlad.capok@mail.ru>
2023-07-03 11:45:43 +0300

    Comlite hw_3_2

---
+++ hw_3/hw_3_2.java
@@ -0,0 +1,29 @@
+// Пусть дан произвольный список целых чисел, удалить из него четные числа
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class hw_3_2 {
+    public static void main(String[] args){
+        Integer[] array = new Integer[]{1, 2, 5, 8, 3, 22};
+        List<Integer> list1 = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]+" ");
+            list1.add(array[i]);
+        }
+        System.out.println();
+
+        // System.out.println(list1);
+
+        for (int i = 0; i < list1.size(); i++) {
+            if (list1.get(i)%2 != 1){
+                // System.out.print(list1.get(i)+" ");
+                list1.remove(i);
+            }
+        }
+        
+        System.out.println(list1);
+
+    }
+}
